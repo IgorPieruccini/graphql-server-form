5fcb90c5ae1191765f45c9d6e4314fdd3577ecba
igor pieruccini <igor.pieruccini@sosafe.de>
2022-10-05 17:33:33 +0200

    load graphql files

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,22 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'graphql';
+import { loadSchemaSync } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 
-// Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+const typeDef = loadSchemaSync('./**/*.graphql', {
+  loaders: [new GraphQLFileLoader()],
+});
 
 // The root provides a resolver function for each API endpoint
 const root = {
   hello: () => 'Hello world!',
+  name: () => 'igor',
+  surname: () => 'Pieruccini',
 };
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
-  schema,
+  schema: typeDef,
   rootValue: root,
   graphiql: true,
 }));
